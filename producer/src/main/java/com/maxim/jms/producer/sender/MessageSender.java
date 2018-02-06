c9f5b8d6efd3df08a8f5e6ea84a2c1f7d4b1212f
deerme.org <info@deerme.org>
2018-02-06 01:22:11 -0300

    adding consumer and producer projects

---
+++ producer/src/main/java/com/maxim/jms/producer/sender/MessageSender.java
@@ -0,0 +1,27 @@
+package com.maxim.jms.producer.sender;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.JmsException;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageSender {
+	private static Logger logger = LogManager.getLogger(MessageSender.class.getName());
+	@Autowired
+	JmsTemplate jmsTemplate;
+	
+	public void send(String json) {
+		try{
+			jmsTemplate.convertAndSend(json);
+			logger.info("Message Sent");
+		}
+		catch(JmsException jmsException)
+		{
+			logger.error("Message: " + json);
+		}
+		
+	}
+}
